19c6bd1369119662f89a068cdee1a7090a130844
Nitansh Rastogi <nitansh@Nitanshs-MacBook-Pro.local>
2026-04-09 23:39:21 +0530

    updste logi quality

--- src/components/features/masaiverse/MasaiverseSidebar/Logo.tsx
+++ src/components/features/masaiverse/MasaiverseSidebar/Logo.tsx
@@ -1,7 +1,10 @@
 const Logo = () => {
   return (
     <div className="w-fit mt-[24px] pb-[24px] border-b border-[#E5E7EB] mx-auto ">
-      <img src="https://masai-drive-uploads-prod.s3.ap-south-1.amazonaws.com/drive/69c5405a1048890fc9f0c63c/1775484635200-b3c638ae99b5387a.png" alt="Masaiverse Logo" />
+      <img
+        src="https://s3.ap-south-1.amazonaws.com/static.masaischool.com/masai-logo.svg"
+        alt="Masaiverse Logo"
+      />
     </div>
   )
 }
