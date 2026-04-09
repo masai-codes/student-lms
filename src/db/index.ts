da1d6e51e98f39d8389811b89a09610676d38c5e
Amit Saharan <asaharan812@gmail.com>
2026-04-09 19:32:25 +0530

    import vars using NITRO_ for backend

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,8 @@
-import 'dotenv/config';
-import { drizzle } from "drizzle-orm/mysql2";
+import 'dotenv/config'
+import { drizzle } from 'drizzle-orm/mysql2'
 
-if(!process.env.DATABASE_URL) {
-    throw new Error("DATABASE_URL is not defined in environment variables");
+if (!process.env.NITRO_DATABASE_URL) {
+  throw new Error('DATABASE_URL is not defined in environment variables')
 }
 
-export const db = drizzle(process.env.DATABASE_URL);
+export const db = drizzle(process.env.NITRO_DATABASE_URL)
