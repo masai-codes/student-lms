5ba5db75e02d5771411cd5db5d5f5e93726779e4
Nitansh Rastogi <nitansh@Nitanshs-MacBook-Pro.local>
2026-04-23 22:08:49 +0530

    log request and response

--- src/server/plugins/ensureSecrets.ts
+++ src/server/plugins/ensureSecrets.ts
@@ -1,8 +1,21 @@
 import { definePlugin } from 'nitro'
 import { ensureSecrets } from '../../secrets'
+import type { HTTPEvent } from 'nitro/h3'
 
 export default definePlugin((nitroApp) => {
-  nitroApp.hooks.hook('request', async () => {
+  nitroApp.hooks.hook('request', async (event:HTTPEvent) => {
+    // const data = await event.req.formData()
+    console.log('event', event.req.method, event.req._url?.pathname)
+    // if(event.req.method === 'POST') {
+      // const data = await event.req.formData()
+      // console.log('data', data)
+    // }
     await ensureSecrets()
+  })
+  nitroApp.hooks.hook('response',  (res:Response, event: HTTPEvent) => {
+    console.log('sending response',event.req.method, event.req._url?.pathname, res.status)
+  })
+  nitroApp.hooks.hook('error', (err:Error) => {
+    console.log('error', JSON.stringify(err.message), JSON.stringify(err.stack))
   })
 })
