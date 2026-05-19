ad71c8e2c9df5e002a78ad0943203f16b4c29f82
Nitansh Rastogi <nitansh@Nitanshs-MacBook-Pro.local>
2026-05-19 18:42:08 +0530

    update pakcage

---
+++ ecosystem.config.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  apps: [
+    {
+      name: 'student-lms-masai',
+      cwd: __dirname,
+      script: '.output-masai/server/index.mjs',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 7090,
+        VITE_APP_ORIGIN: 'masai',
+      },
+    },
+    {
+      name: 'student-lms-ihub',
+      cwd: __dirname,
+      script: '.output-ihub/server/index.mjs',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 7091,
+        VITE_APP_ORIGIN: 'ihub',
+      },
+    },
+  ],
+}
