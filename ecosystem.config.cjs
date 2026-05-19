57e2ff0d5a7477320a490652842ddef51f8d86b8
Nitansh Rastogi <nitansh@Nitanshs-MacBook-Pro.local>
2026-05-19 18:57:25 +0530

    update env

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,3 +1,15 @@
+const path = require('path')
+const dotenv = require('dotenv')
+
+const { parsed: dotEnv = {} } = dotenv.config({
+  path: path.join(__dirname, '.env'),
+})
+
+const sharedEnv = {
+  ...dotEnv,
+  NODE_ENV: 'production',
+}
+
 module.exports = {
   apps: [
     {
@@ -5,7 +17,7 @@ module.exports = {
       cwd: __dirname,
       script: '.output-masai/server/index.mjs',
       env: {
-        NODE_ENV: 'production',
+        ...sharedEnv,
         PORT: 7090,
         VITE_APP_ORIGIN: 'masai',
       },
@@ -15,7 +27,7 @@ module.exports = {
       cwd: __dirname,
       script: '.output-ihub/server/index.mjs',
       env: {
-        NODE_ENV: 'production',
+        ...sharedEnv,
         PORT: 7091,
         VITE_APP_ORIGIN: 'ihub',
       },
