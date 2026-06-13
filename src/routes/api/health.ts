28dbbb2c5200476b5dce7617b33645ab25527d97
Amit Saharan <asaharan812@gmail.com>
2026-06-13 12:34:02 +0530

    send cookie_name in api health check.

--- src/routes/api/health.ts
+++ src/routes/api/health.ts
@@ -3,7 +3,10 @@ import { createFileRoute } from '@tanstack/react-router'
 export const Route = createFileRoute('/api/health')({
   server: {
     handlers: {
-      GET: () => new Response('OK'),
+      GET: () =>
+        new Response(
+          `OK. AuthCookieName: ${process.env.COOKIE_NAME}. Old URI:${process.env.VITE_OLD_STUDENT_UI_URL}`,
+        ),
     },
   },
 })
