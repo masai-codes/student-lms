3a51bb6a2b3651710fc5852d22fc96bc00ce7503
Amit Saharan <asaharan812@gmail.com>
2026-07-29 11:36:30 +0530

    Add domain name parameter overrides to prod deployment

--- scripts/prod-env/update-deployment.sh
+++ scripts/prod-env/update-deployment.sh
@@ -3,4 +3,5 @@ aws cloudformation deploy \
   --stack-name student-lms-pre-prod \
   --capabilities CAPABILITY_NAMED_IAM  --region=ap-south-1 \
   --s3-bucket=cdk-hnb659fds-assets-016530944324-ap-south-1 \
-  --no-execute-changeset
+  --no-execute-changeset \
+  --parameter-overrides AdditionalDomainNames=learn.ihubiitrcourses.org,iitj.masaischool.com
